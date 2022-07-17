e8e95dbcf5a57608247f29045e6b22f1b9d5dbc3
Eric Silva <ericsilvaccp@gmail.com>
2022-07-17 19:58:20 -0300

    feat: descoment code

--- src/data/useCases/login/index.ts
+++ src/data/useCases/login/index.ts
@@ -12,34 +12,28 @@ export class Login implements ExecuteLogin {
   public async onLogin(props: ExecuteLogin.Props): ExecuteLogin.ReturnType {
     const { username, password } = props
     try {
-      // const urlLogin = 'https://blaze.com/pt/?modal=auth&tab=login'
-      // const driver = await this.webDriverBroswer.onCreateDriver()
+      const urlLogin = 'https://blaze.com/pt/?modal=auth&tab=login'
+      const driver = await this.webDriverBroswer.onCreateDriver()
 
-      // await driver.get(urlLogin)
+      await driver.get(urlLogin)
 
-      // await driver
-      //   .findElement(By.xpath(`//input[@name='username']`))
-      //   .sendKeys(username)
-      // await driver
-      //   .findElement(By.xpath(`//input[@name='password']`))
-      //   .sendKeys(password + Key.ENTER)
-
-      console.log(
-        await this.decodeBearerToken.onDecodeBearerToken(
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY3NjQ2NjQsImJsb2NrcyI6W10sImlhdCI6MTY1ODA5NDE1MSwiZXhwIjoxNjYzMjc4MTUxfQ.uLyPDmaVifsBmKBmOzgZ-RNstysJV0TwXBXjtNJ98eQ'
-        )
-      )
+      await driver
+        .findElement(By.xpath(`//input[@name='username']`))
+        .sendKeys(username)
+      await driver
+        .findElement(By.xpath(`//input[@name='password']`))
+        .sendKeys(password + Key.ENTER)
 
       return Promise.resolve({
         accessToken: '',
         expirationTime: ''
       })
     } catch (error) {
-      // console.error(error)
-      // await this.webDriverBroswer.onDestroyDriver()
+      console.error(error)
+      await this.webDriverBroswer.onDestroyDriver()
       throw new Error(error)
     } finally {
-      // await this.webDriverBroswer.onDestroyDriver()
+      await this.webDriverBroswer.onDestroyDriver()
     }
   }
 }
